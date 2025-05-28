217e09f91ea28ad30b92f50ee994f7e081edddc5
Alejandro Naistat <alenaist10@gmail.com>
2025-05-28 13:33:23 -0300

    removed unwanted comments

--- HCP/src/app/home-drawer/home-drawer.component.ts
+++ HCP/src/app/home-drawer/home-drawer.component.ts
@@ -23,17 +23,8 @@ export class HomeDrawerComponent {
   
  constructor(public router: Router) {
     console.log('HomeDrawerComponent initialized with section:', this.section);
-    // Initialization logic can go here if needed
   }
   navigateToTool(toolUrl: any) {
     this.router.navigate([toolUrl]);
   };
-  
-  
-  ngOnInit() {
-    console.log('HomeDrawerComponent ngOnInit called with section:', this.section);
-  }
-  ngOnChanges() {   
-    console.log('HomeDrawerComponent ngOnChanges called with section:', this.section);  
-  }
 }
